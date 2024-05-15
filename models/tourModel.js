e2f18c64f25c4a06a53d9629730662c257ee6490
Cesar Quisquinay <augustoacual@gmail.com>
2024-05-15 17:16:00 -0600

    Aggretagion Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -87,7 +87,13 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`)
-  console.log(docs)
+  next()
+})
+
+// AGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
+  console.log(this.pipeline())
   next()
 })
 
