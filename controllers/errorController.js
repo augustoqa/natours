cb5e578256906fee9100f99b94575c4d6b09b937
Cesar Quisquinay <augustoacual@gmail.com>
2024-07-18 11:48:07 -0600

    Handle Mongoose Validation Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,6 +10,13 @@ const handleDuplicateFieldDB = (err) => {
   return new AppError(message, 400)
 }
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message)
+
+  const message = `Invalid input data. ${errors.join('. ')}`
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -51,6 +58,7 @@ module.exports = (err, req, res, next) => {
 
     if (err.name === 'CastError') error = handleCastErrorDB(error)
     if (err.code === 11000) error = handleDuplicateFieldDB(error)
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error)
 
     sendErrorProd(error, res)
   }
