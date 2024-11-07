9938333f0453151fc8293bb3072778493e53c3d6
Augusto Quisquinay <augustoacual@gmail.com>
2024-11-07 17:26:24 -0600

    Protecting Tour Routes - Part 2

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -17,6 +17,12 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400)
 }
 
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401)
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again', 401)
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -42,7 +48,7 @@ const sendErrorProd = (err, res) => {
     // 2) send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something wnet very wrong!',
+      message: 'Something went very wrong!',
     })
   }
 }
@@ -59,6 +65,8 @@ module.exports = (err, req, res, next) => {
     if (err.name === 'CastError') error = handleCastErrorDB(error)
     if (err.code === 11000) error = handleDuplicateFieldDB(error)
     if (err.name === 'ValidationError') error = handleValidationErrorDB(error)
+    if (err.name === 'JsonWebTokenError') error = handleJWTError()
+    if (err.name === 'TokenExpiredError') error = handleJWTExpiredError()
 
     sendErrorProd(error, res)
   }
